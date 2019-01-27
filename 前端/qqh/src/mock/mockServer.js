4b06b6e6a1e77b5627a7a61773bb640f5143ff98
buhairong <22858111@qq.com>
2019-01-27 17:22:49 +0800

    update home

--- 前端/qqh/src/mock/mockServer.js
+++ 前端/qqh/src/mock/mockServer.js
@@ -1,13 +1,14 @@
 import Mock from 'mockjs'
 
+// 轮播图模拟数据
 let swiper = Mock.mock({
   'list|1-5': [{
     'id|+1': 1,
+    'articleId|1-1000': 1,
     'imgSrc': 'http://qiuqiuhua.com/upload/img/banner1@3x.png'
   }]
 })
 
 console.log(JSON.stringify(swiper))
 
-// 轮播图获取
 Mock.mock('/swiper', {data: swiper.list})
