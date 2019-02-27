8cef033ff3d830adc8dc0c3160ef02dc200061ae
buhairong <22858111@qq.com>
2019-02-27 22:42:55 +0800

    update

--- 前端/qqh/src/pages/PlantAdopt/PlantAdopt.vue
+++ 前端/qqh/src/pages/PlantAdopt/PlantAdopt.vue
@@ -4,28 +4,7 @@
       <span class="title" slot="headerTitle">植物送养</span>
     </Header>
     <div class="content">
-      <div class="search_wrap">
-        <div class="selectBox">
-          <select id="provinceSel" ref="provinceSel">
-            <option value="">全部</option>
-            <option v-for="(item, index) in provinceArr" :key="index" :value="item.provinceName">
-              {{item.provinceName}}
-            </option>
-          </select>
-          <label for="provinceSel">
-            <i class="iconfont icon-iconarrowdown01"></i>
-          </label>
-        </div>
-        <div class="inputBox">
-          <div v-show="!showInput" @click="inputShow">
-            <div>
-              请输入搜索内容
-            </div>
-          </div>
-          <input type="text" v-model="searchText" v-show="showInput" ref="searchInput">
-        </div>
-        <a href="javascript:;">搜索</a>
-      </div>
+      <SearchWrap @searchText="getSearchText"  @provinceSelect="getProvinceSelect"/>
       <div class="pets">
         <div class="wrapper" ref="wrapper">
           <ul>
@@ -57,6 +36,7 @@ import {mapState} from 'vuex'
 import BScroll from 'better-scroll'
 import {reqPlantAdoptInfo} from '../../api'
 import Header from '../../components/Header/Header.vue'
+import SearchWrap from '../../components/SearchWrap/SearchWrap.vue'
 
 export default {
   data () {
@@ -67,13 +47,11 @@ export default {
       totalPage: 0, // 一共几页
       infos: [], // 信息列表
       underText: '', // 底部文本
-      provinceSelect: '', // 当前选中的省市
-      showInput: false
+      provinceSelect: '' // 当前选中的省市
     }
   },
 
   mounted () {
-    this.provinceSelect = this.$refs.provinceSel.value = this.province
     this.getInfos()
     this.$nextTick(() => {
       this.scroll = new BScroll(this.$refs.wrapper, {
@@ -105,8 +83,17 @@ export default {
   },
 
   methods: {
-    inputShow () {
-      this.showInput = true
+    getSearchText (searchText) {
+      this.searchText = searchText
+      console.log('searchText=' + searchText)
+    },
+
+    getProvinceSelect (provinceSelect) {
+      this.provinceSelect = provinceSelect
+      console.log('provinceSelect=' + provinceSelect)
+      this.pageNum = 1
+      this.underText = '加载中...'
+      this.getInfos()
     },
 
     async getInfos () {
@@ -139,12 +126,6 @@ export default {
   },
 
   watch: {
-    showInput () {
-      this.$nextTick(() => {
-        this.$refs.searchInput.focus()
-      })
-    },
-
     infos () {
       setTimeout(() => {
         this.scroll.refresh()
@@ -153,7 +134,8 @@ export default {
   },
 
   components: {
-    Header
+    Header,
+    SearchWrap
   }
 }
 </script>
@@ -167,81 +149,6 @@ export default {
     background:#f0f0f0;
     .content {
       margin-top:44/@rem;
-      .search_wrap {
-        width: 100%;
-        height: 51/@rem;
-        padding: 11/@rem 16/@rem 11/@rem;
-        background:#fff;
-        box-sizing:border-box;
-        .selectBox {
-          float:left;
-          position:relative;
-          background:#fff;
-          width: 61/@rem;
-          height: 29/@rem;
-          select {
-            color:#999;
-            font-size:15/@rem;
-            height: 29/@rem;
-            line-height:29/@rem;
-            background:#fff;
-          }
-          label {
-            float:right;
-            i {
-              font-size:11.3/@rem;
-              color:#999;
-              line-height:29/@rem;
-            }
-          }
-        }
-        .inputBox {
-          float:left;
-          margin-left:17/@rem;
-          width:219/@rem;
-          height:29/@rem;
-          border-radius:14.5/@rem;
-          > div {
-            width:100%;
-            text-align:center;
-            background:#f0f0f0;
-            border-radius:14.5/@rem;
-            > div {
-              display:inline-block;
-              color:#999;
-              height:29/@rem;
-              line-height:29/@rem;
-              font-size:13/@rem;
-              background-size:12/@rem 12/@rem;
-              background-position: 0 8/@rem;
-              background-repeat: no-repeat;
-              padding-left: 15/@rem;
-              .bg-image('./images/sousuo');
-            }
-          }
-          input {
-            display:block;
-            width:100%;
-            box-sizing:border-box;
-            color:#333;
-            font-size:13/@rem;
-            height:29/@rem;
-            line-height:29/@rem;
-            background:#f0f0f0;
-            padding:0 10/@rem;
-            border-radius:14.5/@rem;
-            &::-webkit-input-placeholder{
-              color:#999;
-            }
-          }
-        }
-        > a {
-          float:right;
-          line-height:29/@rem;
-          font-size:15/@rem;
-          color:#999;
-        }
-      }
       .wrapper {
         position: absolute;
         left: 0;
